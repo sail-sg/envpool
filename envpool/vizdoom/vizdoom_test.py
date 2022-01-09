10bfbd94d7f6a65e6ef22166508e3416480fa4b4
Jiayi Weng <wengj@sea.com>
2022-01-09 16:12:17 -0500

    fix ci

--- envpool/vizdoom/vizdoom_test.py
+++ envpool/vizdoom/vizdoom_test.py
@@ -11,4 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Unit tests for vizdoom environments."""
 
+from absl.testing import absltest
+
+if __name__ == "__main__":
+  absltest.main()
