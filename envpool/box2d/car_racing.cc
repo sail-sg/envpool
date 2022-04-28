ec8dc4b70e70306faed8c3e893fd94aede345f5a
Alicia <alicialuo1529@gmail.com>
2022-04-28 01:50:06 +0000

    more car initialization

--- envpool/box2d/car_racing.cc
+++ envpool/box2d/car_racing.cc
@@ -83,17 +83,60 @@ Car::Car(b2World* _world, float init_angle, float init_x, float init_y)
         hull = world->CreateBody(&bd);
 
         b2PolygonShape polygon1 = generatePolygon(kHullPoly1, 4);
-        fixtures.emplace_back(hull->CreateFixture(&polygon1, 1.f));
+        hullFixtures.emplace_back(hull->CreateFixture(&polygon1, 1.f));
 
         b2PolygonShape polygon2 = generatePolygon(kHullPoly2, 4);
-        fixtures.emplace_back(hull->CreateFixture(&polygon2, 1.f));
+        hullFixtures.emplace_back(hull->CreateFixture(&polygon2, 1.f));
 
         b2PolygonShape polygon3 = generatePolygon(kHullPoly3, 8);
-        fixtures.emplace_back(hull->CreateFixture(&polygon3, 1.f));
+        hullFixtures.emplace_back(hull->CreateFixture(&polygon3, 1.f));
 
         b2PolygonShape polygon4 = generatePolygon(kHullPoly4, 4);
-        fixtures.emplace_back(hull->CreateFixture(&polygon4, 1.f));
-   
+        hullFixtures.emplace_back(hull->CreateFixture(&polygon4, 1.f));
+
+        for (int i = 0; i < 8; i += 2) {
+          float wx = kWheelPos[i], wy = kWheelPos[i+1];
+          float front_k = 1.0;
+
+          b2BodyDef bd;
+          bd.position.Set(init_x + wx * kSize, init_y + wy * kSize);
+          bd.angle = init_angle;
+          bd.type = b2_dynamicBody;
+          auto w = world->CreateBody(&bd);
+
+          float vertices[8];
+          for (int j = 0; j < 8; j+= 2) {
+            vertices[j] = wheelPoly[j] * front_k * kSize;
+            vertices[j + 1] = wheelPoly[j+1] * front_k * kSize;
+          }
+
+          b2PolygonShape polygon = generatePolygon(vertices, 4);
+          b2FixtureDef* def = new b2FixtureDef();
+
+          def->shape = &polygon;
+          def->density = 0.1f;
+          def->filter.categoryBits = 0x0020;
+          def->filter.maskBits=0x001;
+          def->restitution=0.0;
+
+          w->CreateFixture(def);
+
+          CarData* carData = new CarData();
+          carData->wheel_rad = front_k * kWheelR * kSize;
+          memcpy(carData->color, kWheelColor, 3 * sizeof(float));
+          carData->gas = 0;
+          carData->brake = 0;
+          carData->steer = 0;
+          carData->phase = 0.0;
+          carData->omega = 0.0;
+          // carData->skid_start
+          // carData->skid_particle
+          // carData->joint
+          // carData->tiles
+          carData->userData = w;
+          w->SetUserData(carData);
+          wheels.emplace_back(w);
+        }
     }
 
 }  // namespace box2d
