45c7ef847d11e217559b5198d24ada5fa03a2cfe
Jiayi Weng <trinkle23897@gmail.com>
2022-05-02 16:24:37 -0400

    update benchmark script

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -48,7 +48,9 @@ import envpool
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("--task", type=str, default="Pong-v5")
+  parser.add_argument(
+    "--env", type=str, default="atari", choices=["atari", "mujoco", "vizdoom"]
+  )
   parser.add_argument("--num-envs", type=int, default=645)
   parser.add_argument("--batch-size", type=int, default=248)
   # num_threads == 0 means to let envpool itself determine
@@ -56,22 +58,33 @@ if __name__ == "__main__":
   # thread_affinity_offset == -1 means no thread affinity
   parser.add_argument("--thread-affinity-offset", type=int, default=0)
   parser.add_argument("--total-iter", type=int, default=50000)
+  parser.add_argument("--seed", type=int, default=0)
   args = parser.parse_args()
-  env = envpool.make_gym(
-    args.task,
+  task = {
+    "atari": "Pong-v5",
+    "mujoco": "Ant-v3",
+    "vizdoom": "HealthGathering-v1",
+  }
+  kwargs = dict(
     num_envs=args.num_envs,
     batch_size=args.batch_size,
     num_threads=args.num_threads,
     thread_affinity_offset=args.thread_affinity_offset,
-    use_inter_area_resize=False,
   )
+  if task in ["atari", "vizdoom"]:
+    kwargs.update(use_inter_area_resize=False)
+  env = envpool.make_gym(task[args.env], **kwargs)
   env.async_reset()
-  action = np.ones(args.batch_size, dtype=np.int32)
+  env.action_space.seed(args.seed)
+  action = np.array(
+    [env.action_space.sample() for _ in range(args.batch_size)]
+  )
   t = time.time()
   for _ in range(args.total_iter):
     info = env.recv()[-1]
     env.send(action, info["env_id"])
   duration = time.time() - t
-  fps = args.total_iter * args.batch_size / duration * 4
+  frame_skip = getattr(env.spec.config, "frame_skip", 1)
+  fps = args.total_iter * args.batch_size / duration * frame_skip
   print(f"Duration = {duration:.2f}s")
   print(f"EnvPool FPS = {fps:.2f}")
