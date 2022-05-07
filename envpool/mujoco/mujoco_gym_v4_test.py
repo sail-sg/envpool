a240fc6408650e8cce0f7026da4611f5e63d5d70
Jiayi Weng <trinkle23897@gmail.com>
2022-05-07 11:53:55 -0400

    reorg

--- envpool/mujoco/mujoco_gym_v4_test.py
+++ envpool/mujoco/mujoco_gym_v4_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Unit tests for Mujoco environments."""
+"""Unit tests for Mujoco gym v4 environments."""
 
 from typing import Any, no_type_check
 
