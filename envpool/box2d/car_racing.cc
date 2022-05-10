bc1a33656d855aa8c2dbe5f205797eb7278a1cd9
Alicia <alicialuo1529@gmail.com>
2022-05-10 14:33:51 +0000

    add car_dynamics

--- envpool/box2d/car_racing.cc
+++ envpool/box2d/car_racing.cc
@@ -16,6 +16,14 @@
 // https://github.com/openai/gym/blob/master/gym/envs/box2d/car_racing.py
 
 #include "car_racing.h"
+#include <math.h>  
+
+float min(float a, float b) {
+  return (a > b) ? b: a;
+}
+float max(float a, float b) {
+  return (a > b) ? a: b;
+}
 
 namespace box2d {
 
@@ -102,7 +110,6 @@ Car::Car(b2World* _world, float init_angle, float init_x, float init_y)
           bd.position.Set(init_x + wx * kSize, init_y + wy * kSize);
           bd.angle = init_angle;
           bd.type = b2_dynamicBody;
-          auto w = world->CreateBody(&bd);
 
           float vertices[8];
           for (int j = 0; j < 8; j+= 2) {
@@ -119,24 +126,146 @@ Car::Car(b2World* _world, float init_angle, float init_x, float init_y)
           def->filter.maskBits=0x001;
           def->restitution=0.0;
 
-          w->CreateFixture(def);
+          WheelData* wheelData = new WheelData;
+          wheelData->wheel = world->CreateBody(&bd);
+          wheelData->wheel->CreateFixture(def);
 
-          CarData* carData = new CarData();
-          carData->wheel_rad = front_k * kWheelR * kSize;
-          memcpy(carData->color, kWheelColor, 3 * sizeof(float));
-          carData->gas = 0;
-          carData->brake = 0;
-          carData->steer = 0;
-          carData->phase = 0.0;
-          carData->omega = 0.0;
-          // carData->skid_start
-          // carData->skid_particle
-          // carData->joint
-          // carData->tiles
-          carData->userData = w;
-          w->SetUserData(carData);
-          wheels.emplace_back(w);
+          wheelData->wheel_rad = front_k * kWheelR * kSize;
+          memcpy(wheelData->color, kWheelColor, 3 * sizeof(float));
+          wheelData->gas = 0;
+          wheelData->brake = 0;
+          wheelData->steer = 0;
+          wheelData->phase = 0.0;
+          wheelData->omega = 0.0;
+          wheelData->skid_start = NULL;
+          wheelData->skid_particle = NULL;
+
+          b2RevoluteJointDef rjd;
+          rjd.bodyA = hull;
+          rjd.bodyB = wheelData->wheel;
+          rjd.localAnchorA.Set(wx * kSize, wy * kSize);
+          rjd.localAnchorB.Set(0, 0);
+          rjd.enableMotor = true;
+          rjd.enableLimit = true;
+          rjd.maxMotorTorque = 180 * 900 * kSize * kSize;
+          rjd.motorSpeed = 0;
+          rjd.lowerAngle = -0.4;
+          rjd.upperAngle = +0.4;
+          b2Joint* joint = world->CreateJoint(&rjd);
+          wheelData->joint = dynamic_cast<b2RevoluteJoint *>(joint);
+          // world->CreateJoint(&rjd);
+          wheelData->wheel->SetUserData(wheelData);
+          wheels.emplace_back(wheelData);
         }
     }
+void Car::gas(float g) {
+  if (g < 0) g = 0;
+  if (g > 1) g = 1;
+  for (int i = 2; i < 4; i++) {
+    auto w = wheels[i];
+    auto diff = g - w->gas;
+    if (diff > 0.1) diff = 0.1;
+    w->gas += diff;
+  }
 
+}
+void Car::brake(float b) {
+  for (auto& w: wheels) {
+    w->brake = b;
+  }
+}
+void Car::steer(float s) {
+  wheels[0]->steer = s;
+  wheels[1]->steer = s;
+}
+void Car::step(float dt) {
+  for (auto w: wheels) {
+    // Steer each wheel
+    float dir = (w->steer - w->joint->GetJointAngle() > 0) ? 1: -1;
+    float val = abs(w->steer - w->joint->GetJointAngle() > 0);
+    w->joint->SetMotorSpeed(dir * min(50.0 * val, 3.0));
+    // Position => friction_limit
+    auto grass = true;
+    auto friction_limit = kFrictionLimit * 0.6;  // Grass friction if no tile
+    for (auto t: w->tiles) {
+      friction_limit = max(
+          friction_limit, kFrictionLimit * t->roadFriction
+      );
+      grass = false;
+    }
+    // Force
+    auto forw = w->wheel->GetWorldVector({0, 1});
+    auto side = w->wheel->GetWorldVector({1, 0});
+    auto v = w->wheel->GetLinearVelocity();
+    auto vf = forw.x * v.x + forw.y * v.y;  // forward speed
+    auto vs = side.x * v.x + side.y * v.y;  // side speed
+    // WHEEL_MOMENT_OF_INERTIA*np.square(w.omega)/2 = E -- energy
+    // WHEEL_MOMENT_OF_INERTIA*w.omega * domega/dt = dE/dt = W -- power
+    // domega = dt*W/WHEEL_MOMENT_OF_INERTIA/w.omega
+
+    // add small coef not to divide by zero
+    w->omega += (
+        dt * kEnginePower * w->gas / kWheelMomentOfInertia/ (abs(w->omega) + 5.0)
+    );
+    fuel_spent += dt * kEnginePower * w->gas;
+
+    if (w->brake >= 0.9) {
+      w->omega = 0;
+    } else if (w->brake > 0) {
+      auto brake_force = 15;  // radians per second
+      dir = (w->omega > 0) ? -1: 1; // -np.sign(w.omega)
+      val = brake_force * w->brake;
+      if (abs(val) > abs(w->omega)) {
+        val = abs(w->omega); // low speed => same as = 0
+      }
+      w->omega += dir * val;
+    }
+    w->phase += w->omega * dt;
+
+    auto vr = w->omega * w->wheel_rad;  // rotating wheel speed
+    auto f_force = -vf + vr;  // force direction is direction of speed difference
+    auto p_force = -vs;
+
+    // Physically correct is to always apply friction_limit until speed is equal.
+    // But dt is finite, that will lead to oscillations if difference is already near zero.
+
+    // Random coefficient to cut oscillations in few steps (have no effect on friction_limit)
+    f_force *= 205000 * kSize * kSize;
+    p_force *= 205000 * kSize * kSize;
+    auto force = sqrt(pow(f_force, 2) + pow(p_force, 2));
+
+    // Skid trace
+    if (abs(force) > 2.0 * friction_limit) {
+        if (w->skid_particle != NULL && w->skid_particle->isGrass == grass && w->skid_particle->poly.size() < 30) {
+          w->skid_particle->poly.push_back({w->wheel->GetPosition().x, w->wheel->GetPosition().y});
+        } else if (w->skid_start == NULL) {
+          w->skid_start = new b2Vec2(w->wheel->GetPosition());
+        } else {
+          w->skid_particle = createParticle(*(w->skid_start), w->wheel->GetPosition(), grass);
+          w->skid_start = NULL;
+        }
+    } else {
+      w->skid_start = NULL;
+      w->skid_particle = NULL;
+    }
+
+    if (abs(force) > friction_limit) {
+        f_force /= force;
+        p_force /= force;
+        force = friction_limit;  // Correct physics here
+        f_force *= force;
+        p_force *= force;
+    }
+
+
+    w->omega -= dt * f_force * w->wheel_rad / kWheelMomentOfInertia;
+
+    w->wheel->ApplyForceToCenter(
+        {
+            p_force * side.x + f_force * forw.x,
+            p_force * side.y + f_force * forw.y,
+        },
+        true);
+  }
+}
 }  // namespace box2d
--- envpool/box2d/car_racing.h
+++ envpool/box2d/car_racing.h
@@ -33,6 +33,27 @@ class CarRacingEnvFns;
 class CarRacingEnv;
 
 
+static float kRoadColor[3] = {0.4, 0.4, 0.4};
+
+static float kSize = 0.02;
+static float kEnginePower = 100000000 * kSize * kSize;
+static float kWheelMomentOfInertia = 4000 * kSize * kSize;
+static float kFrictionLimit = 1000000 * kSize * kSize;
+// friction ~= mass ~= size^2 (calculated implicitly using density)
+static float kWheelR = 27;
+static float kWheelW = 14;
+// WHEELPOS = [(-55, +80), (+55, +80), (-55, -82), (+55, -82)]
+static float kWheelPos[8] = {-55, +80, +55, +80, -55, -82, +55, -82};
+static float kHullPoly1[8] = {-60, +130, +60, +130, +60, +110, -60, +110};
+static float kHullPoly2[8] = {-15, +120, +15, +120, +20, +20, -20, 20};
+static float kHullPoly3[16] = {+25, +20, +50, -10, +50, -40, +20, -90, -20, -90, -50, -40, -50, -10, -25, +20};
+static float kHullPoly4[8] = {-50, -120, +50, -120, +50, -90, -50, -90};
+
+static float kWheelColor[3] = {0.0, 0.0, 0.0};
+static float kWheelWhite[3] = {0.3, 0.3, 0.3};
+static float kMudColor[3] = {0.4, 0.4, 0.0};
+
+
 typedef struct UserData{
   b2Body* body;
   bool isTile;
@@ -42,24 +63,40 @@ typedef struct UserData{
   std::unordered_set<struct UserData*> objTiles;
 } UserData;
 
-float kRoadColor[3] = {0.4, 0.4, 0.4};
 
-float kSize = 0.02;
-float kEnginePower = 100000000 * kSize * kSize;
-float kWheelMomentOfInertia = 4000 * kSize * kSize;
-float kFrictionLimit = 1000000 * kSize * kSize;
-// friction ~= mass ~= size^2 (calculated implicitly using density)
-int kWheelR = 27;
-int kWheelW = 14;
-// WHEELPOS = [(-55, +80), (+55, +80), (-55, -82), (+55, -82)]
-float kHullPoly1[8] = {-60, +130, +60, +130, +60, +110, -60, +110};
-float kHullPoly2[8] = {-15, +120, +15, +120, +20, +20, -20, 20};
-float kHullPoly3[16] = {+25, +20, +50, -10, +50, -40, +20, -90, -20, -90, -50, -40, -50, -10, -25, +20};
-float kHullPoly4[8] = {-50, -120, +50, -120, +50, -90, -50, -90};
+class Particle {
+  public:
+    float color[3];
+    float ttl;
+    std::vector<std::tuple<float, float> > poly;
+    bool isGrass;
+    Particle(b2Vec2 point1, b2Vec2 point2, bool _isGrass) {
+      if (!_isGrass) {
+        memcpy(color, kWheelColor, 3 * sizeof(float));
+      } else {
+        memcpy(color, kMudColor, 3 * sizeof(float));
+      }
+      ttl = 1;
+      poly.push_back({point1.x, point1.y});
+      poly.push_back({point2.x, point2.y});
+      isGrass = _isGrass;
+    };
+};
 
-float kWheelColor[3] = {0.0, 0.0, 0.0};
-float kWheelWhite[3] = {0.3, 0.3, 0.3};
-float kMudColor[3] = {0.4, 0.4, 0.0};
+typedef struct WheelData{
+  float wheel_rad;
+  float color[3];
+  float gas;
+  float brake;
+  float steer;
+  float phase;
+  float omega;
+  b2Vec2* skid_start;
+  Particle* skid_particle;
+  b2RevoluteJoint *joint;
+  std::unordered_set<UserData*> tiles;
+  b2Body* wheel;
+} WheelData;
 
 b2PolygonShape generatePolygon(float* array, int size);
 
@@ -75,15 +112,31 @@ class FrictionDetector: public b2ContactListener {
 
 };
 
+
 class Car {
   public:
     Car(b2World* _world, float init_angle, float init_x, float init_y);
+    void gas(float g);
+    void brake(float b);
+    void steer(float s);
+    void step(float dt);
   protected:
     b2World* world;
     b2Body* hull;
-    std::vector<b2Body*> wheels;
-    std::vector<b2Fixture*> fixtures;
-    float fuel_spent;
+    std::vector<WheelData*> wheels;
+    std::vector<b2Fixture*> hullFixtures;
+    std::vector<Particle*> particles;
+    float fuel_spent = 0.f;
+    const float color[3] = {0.8, 0.0, 0.0};
+    const float wheelPoly[8] = {-kWheelW, +kWheelR, +kWheelW, +kWheelR, +kWheelW, -kWheelR, -kWheelW, -kWheelR};
+    Particle* createParticle(b2Vec2 point1, b2Vec2 point2, bool isGrass) {
+      Particle* p = new Particle(point1, point2, isGrass);
+      particles.push_back(p);
+      while (particles.size() > 30) {
+        particles.erase(particles.begin());
+      }
+      return p;
+    };
 };
 
 
