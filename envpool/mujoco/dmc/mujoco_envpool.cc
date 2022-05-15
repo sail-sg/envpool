d6173c702eeaadac785554545fd5a27e5e2c7ae9
Jiayi Weng <trinkle23897@gmail.com>
2022-05-15 15:25:54 -0400

    sort

--- envpool/mujoco/dmc/mujoco_envpool.cc
+++ envpool/mujoco/dmc/mujoco_envpool.cc
@@ -18,21 +18,21 @@
 #include "envpool/mujoco/dmc/reacher.h"
 #include "envpool/mujoco/dmc/walker.h"
 
-using DmcHopperEnvSpec = PyEnvSpec<mujoco_dmc::HopperEnvSpec>;
-using DmcHopperEnvPool = PyEnvPool<mujoco_dmc::HopperEnvPool>;
-
 using DmcCheetahEnvSpec = PyEnvSpec<mujoco_dmc::CheetahEnvSpec>;
 using DmcCheetahEnvPool = PyEnvPool<mujoco_dmc::CheetahEnvPool>;
 
-using DmcWalkerEnvSpec = PyEnvSpec<mujoco_dmc::WalkerEnvSpec>;
-using DmcWalkerEnvPool = PyEnvPool<mujoco_dmc::WalkerEnvPool>;
+using DmcHopperEnvSpec = PyEnvSpec<mujoco_dmc::HopperEnvSpec>;
+using DmcHopperEnvPool = PyEnvPool<mujoco_dmc::HopperEnvPool>;
 
 using DmcReacherEnvSpec = PyEnvSpec<mujoco_dmc::ReacherEnvSpec>;
 using DmcReacherEnvPool = PyEnvPool<mujoco_dmc::ReacherEnvPool>;
 
+using DmcWalkerEnvSpec = PyEnvSpec<mujoco_dmc::WalkerEnvSpec>;
+using DmcWalkerEnvPool = PyEnvPool<mujoco_dmc::WalkerEnvPool>;
+
 PYBIND11_MODULE(mujoco_dmc_envpool, m) {
   REGISTER(m, DmcCheetahEnvSpec, DmcCheetahEnvPool)
   REGISTER(m, DmcHopperEnvSpec, DmcHopperEnvPool)
-  REGISTER(m, DmcWalkerEnvSpec, DmcWalkerEnvPool)
   REGISTER(m, DmcReacherEnvSpec, DmcReacherEnvPool)
+  REGISTER(m, DmcWalkerEnvSpec, DmcWalkerEnvPool)
 }
