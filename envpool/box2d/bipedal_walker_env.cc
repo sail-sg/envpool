9dcd92dd84c582e02bfbca3e4afbf9ff94ea9fb2
Alicia <alicialuo1529@gmail.com>
2022-05-18 16:33:46 -0400

    fix test

--- envpool/box2d/bipedal_walker_env.cc
+++ envpool/box2d/bipedal_walker_env.cc
@@ -13,3 +13,70 @@
 // limitations under the License.
 
 #include "envpool/box2d/bipedal_walker_env.h"
+
+namespace box2d {
+
+BipedalWalkerContactDetector::BipedalWalkerContactDetector(
+    BipedalWalkerBox2dEnv* env)
+    : env_(env) {}
+
+void BipedalWalkerContactDetector::BeginContact(b2Contact* contact) {
+  b2Body* body_a = contact->GetFixtureA()->GetBody();
+  b2Body* body_b = contact->GetFixtureB()->GetBody();
+  if (env_->hull_ == body_a || env_->hull_ == body_b) {
+    env_->done_ = true;
+  }
+  if (env_->legs_[1] == body_a || env_->legs_[1] == body_b) {
+    env_->ground_contact_[1] = 1;
+  }
+  if (env_->legs_[3] == body_a || env_->legs_[3] == body_b) {
+    env_->ground_contact_[3] = 1;
+  }
+}
+
+void BipedalWalkerContactDetector::EndContact(b2Contact* contact) {
+  b2Body* body_a = contact->GetFixtureA()->GetBody();
+  b2Body* body_b = contact->GetFixtureB()->GetBody();
+  if (env_->legs_[1] == body_a || env_->legs_[1] == body_b) {
+    env_->ground_contact_[1] = 0;
+  }
+  if (env_->legs_[3] == body_a || env_->legs_[3] == body_b) {
+    env_->ground_contact_[3] = 0;
+  }
+}
+
+BipedalWalkerBox2dEnv::BipedalWalkerBox2dEnv(bool hardcore,
+                                             int max_episode_steps)
+    : max_episode_steps_(max_episode_steps),
+      elapsed_step_(max_episode_steps + 1),
+      hardcore_(hardcore),
+      done_(true),
+      dist_uniform_(0, 1),
+      world_(new b2World(b2Vec2(0.0, -10.0))),
+      hull_(nullptr) {}
+
+void BipedalWalkerBox2dEnv::ResetBox2d(std::mt19937* gen) {}
+
+void BipedalWalkerBox2dEnv::StepBox2d(std::mt19937* gen, float action0,
+                                      float action1, float action2,
+                                      float action3) {
+  if (elapsed_step_ >= max_episode_steps_) {
+    done_ = true;
+  }
+}
+
+void BipedalWalkerBox2dEnv::BipedalWalkerReset(std::mt19937* gen) {
+  elapsed_step_ = -1;  // because of the step(0)
+  done_ = false;
+  ResetBox2d(gen);
+  BipedalWalkerStep(gen, 0, 0, 0, 0);
+}
+
+void BipedalWalkerBox2dEnv::BipedalWalkerStep(std::mt19937* gen, float action0,
+                                              float action1, float action2,
+                                              float action3) {
+  ++elapsed_step_;
+  StepBox2d(gen, action0, action1, action2, action3);
+}
+
+}  // namespace box2d
--- envpool/box2d/bipedal_walker_env.h
+++ envpool/box2d/bipedal_walker_env.h
@@ -30,12 +30,37 @@ namespace box2d {
 class BipedalWalkerContactDetector;
 
 class BipedalWalkerBox2dEnv {
+  const double kFPS = 50;
+  const double kScale = 30.0;
+  const double kMotorsTorque = 80;
+  const double kSpeedHip = 4;
+  const double kSpeedKnee = 6;
+  const double kLidarRange = 160 / kScale;
+  const double kInitialRandom = 5;
+  const double kHullPoly[5][2] = {  // NOLINT
+      {-30, 9},
+      {6, 9},
+      {34, 1},
+      {34, -8},
+      {-30, -8}};
+  const double kLegDown = -8 / kScale;
+  const double kLegW = 8 / kScale;
+  const double kLegH = 34 / kScale;
+  const double kViewportW = 600;
+  const double kViewportH = 400;
+  const double kTerrainStep = 14 / kScale;
+  const double kTerrainLength = 200;
+  const double kTerrainHeight = kViewportH / kScale / 4;
+  const double kTerrainGrass = 10;
+  const double kTerrainStartpad = 20;
+  const double kFriction = 2.5;
+
   friend class BipedalWalkerContactDetector;
 
  protected:
   int max_episode_steps_, elapsed_step_;
   float reward_;
-  bool is_hardcore_, done_;
+  bool hardcore_, done_;
   std::array<float, 24> obs_;
   std::uniform_real_distribution<> dist_uniform_;
 
@@ -47,7 +72,7 @@ class BipedalWalkerBox2dEnv {
   std::unique_ptr<BipedalWalkerContactDetector> listener_;
 
  public:
-  BipedalWalkerBox2dEnv(bool is_hardcore, int max_episode_steps);
+  BipedalWalkerBox2dEnv(bool hardcore, int max_episode_steps);
   void BipedalWalkerReset(std::mt19937* gen);
   void BipedalWalkerStep(std::mt19937* gen, float action0, float action1,
                          float action2, float action3);
