95006d5671a5dd7baea9c0a247d0e5683fbf1eb8
linmin <linmin@sea.com>
2022-06-14 13:24:29 +0800

    Generic code for both np & jnp

--- envpool/python/dm_envpool.py
+++ envpool/python/dm_envpool.py
@@ -78,9 +78,11 @@ class DMEnvPoolMeta(ABCMeta):
       done = state.done
       elapse = state.elapsed_step
       discount = getattr(state, "discount", (1.0 - done).astype(np.float32))
-      step_type = np.full(done.shape, dm_env.StepType.MID)
-      step_type[(elapse == 0)] = dm_env.StepType.FIRST
-      step_type[done] = dm_env.StepType.LAST
+      step_type = (
+        (elapse == 0) * dm_env.StepType.FIRST +
+        ((elapse > 0) & ~done) * dm_env.StepType.MID +
+        done * dm_env.StepType.LAST
+      )
       timestep = TimeStep(
         step_type=step_type,
         observation=state.State,
