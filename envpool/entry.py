2bb4690f9bd5785a531f6d1f0b69da74abea2a6d
kfq2002 <95134838+kfq2002@users.noreply.github.com>
2022-08-03 17:43:00 +0800

    Update entry.py

--- envpool/entry.py
+++ envpool/entry.py
@@ -20,3 +20,4 @@ import envpool.mujoco.dmc.registration  # noqa: F401
 import envpool.mujoco.gym.registration  # noqa: F401
 import envpool.toy_text.registration  # noqa: F401
 import envpool.vizdoom.registration  # noqa: F401
+import envpool.gfootball.registration
