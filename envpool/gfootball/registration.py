ba87f41a5ef8dcd7864a7450afd554f1d47a79e1
kfq2002 <95134838+kfq2002@users.noreply.github.com>
2022-08-10 21:56:35 +0800

    Update registration.py

--- envpool/gfootball/registration.py
+++ envpool/gfootball/registration.py
@@ -4,6 +4,5 @@ register(
     task_id = "football",
     import_path = "envpool.gfootball",
     spec_cls = "FootballEnvSpec",
-    dm_cls = "FootballDMEnvpool",
-    gym_cls = "FootballGymEnvpool",
+    env_cls = "FootballEnvpool",
 )
