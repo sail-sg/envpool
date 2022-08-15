3e4169c809bd06c4a7fbcade9df6e61d89aed112
Benjamin-eecs <benjaminliu.eecs@gmail.com>
2022-08-15 20:40:33 +0800

    fix: update mujoco env arguments

--- envpool/mujoco/dmc/mujoco_env.cc
+++ envpool/mujoco/dmc/mujoco_env.cc
@@ -25,10 +25,9 @@
 namespace mujoco_dmc {
 
 MujocoEnv::MujocoEnv(const std::string& base_path, const std::string& raw_xml,
-                     int n_sub_steps, int max_episode_steps,
-                     const int height, const int,
-                     const std::string& camera_id,
-                     bool depth, bool segmentation)
+                     int n_sub_steps, int max_episode_steps, const int height,
+                     const int, const std::string& camera_id, bool depth,
+                     bool segmentation)
     : n_sub_steps_(n_sub_steps),
       max_episode_steps_(max_episode_steps),
       elapsed_step_(max_episode_steps + 1),
