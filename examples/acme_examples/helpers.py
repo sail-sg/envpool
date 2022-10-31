e4862ad855ea1aee1471cc9d2586a6782287ef61
Jiayi Weng <trinkle23897@gmail.com>
2022-10-30 17:25:31 -0700

    Apply suggestions from code review

--- examples/acme_examples/helpers.py
+++ examples/acme_examples/helpers.py
@@ -279,9 +279,9 @@ class BatchEnvWrapper(dm_env.Environment):
   def reset(self) -> TimeStep:
     self._reset_next_step = False
     if is_legacy_gym:
-      observation, _ = self._environment.reset()
-    else:
       observation = self._environment.reset()
+    else:
+      observation, _ = self._environment.reset()
     ts = TimeStep(
       step_type=np.full(self._num_envs, dm_env.StepType.FIRST, dtype="int32"),
       reward=np.zeros(self._num_envs, dtype="float32"),
--- examples/cleanrl_examples/ppo_atari_envpool.py
+++ examples/cleanrl_examples/ppo_atari_envpool.py
@@ -225,9 +225,9 @@ class RecordEpisodeStatistics(gym.Wrapper):
 
   def reset(self, **kwargs):
     if is_legacy_gym:
-      observations, _ = super(RecordEpisodeStatistics, self).reset(**kwargs)
-    else:
       observations = super(RecordEpisodeStatistics, self).reset(**kwargs)
+    else:
+      observations, _ = super(RecordEpisodeStatistics, self).reset(**kwargs)
     self.episode_returns = np.zeros(self.num_envs, dtype=np.float32)
     self.episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
     self.lives = np.zeros(self.num_envs, dtype=np.int32)
