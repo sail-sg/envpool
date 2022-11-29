14a25cec44c3efab3a0581b39eb2b7466dd41f4d
Jiayi Weng <trinkle23897@gmail.com>
2022-11-28 20:09:45 -0800

    Add back contact force in humanoid-v3 (#224)
    close #143

--- envpool/mujoco/gym/registration.py
+++ envpool/mujoco/gym/registration.py
@@ -48,7 +48,7 @@ gym_mujoco_envs = [
 
 for task, version, post_constraint, max_episode_steps in gym_mujoco_envs:
   extra_args = {}
-  if task == "Ant" and version == "v3":
+  if task in ["Ant", "Humanoid"] and version == "v3":
     extra_args["use_contact_force"] = True
   register(
     task_id=f"{task}-{version}",
