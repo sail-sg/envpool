1e514d32efddfa45b9626f1ae363a890235ec9ef
Jiayi Weng <trinkle23897@gmail.com>
2023-02-15 06:59:38 +0000

    fix

--- envpool/classic_control/pendulum.h
+++ envpool/classic_control/pendulum.h
@@ -77,8 +77,9 @@ class PendulumEnv : public Env<PendulumEnvSpec> {
   void Step(const Action& action) override {
     done_ = (++elapsed_step_ >= max_episode_steps_);
     float act = action["action"_];
-    double u =
-        act < -kMaxTorque ? -kMaxTorque : act > kMaxTorque ? kMaxTorque : act;
+    double u = act < -kMaxTorque  ? -kMaxTorque
+               : act > kMaxTorque ? kMaxTorque
+                                  : act;
     double cost =
         theta_ * theta_ + 0.1 * theta_dot_ * theta_dot_ + 0.001 * u * u;
     double new_theta_dot =
@@ -86,9 +87,9 @@ class PendulumEnv : public Env<PendulumEnvSpec> {
     if (version_ == 0) {
       theta_ += new_theta_dot * kDt;
     }
-    theta_dot_ = new_theta_dot < -kMaxSpeed
-                     ? -kMaxSpeed
-                     : new_theta_dot > kMaxSpeed ? kMaxSpeed : new_theta_dot;
+    theta_dot_ = new_theta_dot < -kMaxSpeed  ? -kMaxSpeed
+                 : new_theta_dot > kMaxSpeed ? kMaxSpeed
+                                             : new_theta_dot;
     if (version_ == 1) {
       theta_ += new_theta_dot * kDt;
     }
