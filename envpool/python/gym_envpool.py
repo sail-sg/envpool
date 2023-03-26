sail-sg/envpool

commit 2788eb9b1b6bec8567e5a92d133ce4fba8d888d6

deps: replace `treevalue` with `optree` (#247)
## Description

- close #246 
- further speedup
- [support custom node type
registration](https://github.com/pytorch/pytorch/issues/65761#issuecomment-1334746366)

## Motivation and Context

<!--Why is this change required? What problem does it solve?-->
<!--If it fixes an open issue, please link to the issue here.-->
<!--You can use the syntax `close #233` if this solves the issue #233-->

Initial Test Results:

### Test gym

TreeValue:

<img width="1073" alt="image"
src="https://user-images.githubusercontent.com/32269413/221405036-ed1d1692-714b-4af7-a011-8accb36b8427.png">

OpTree:

<img width="1074" alt="image"
src="https://user-images.githubusercontent.com/32269413/221405130-521b48fe-7760-4ef0-8abb-bc9468c7bdeb.png">

### Test dmc

TreeValue:

<img width="1061" alt="image"
src="https://user-images.githubusercontent.com/32269413/221405292-69f2b96e-d590-42a5-b073-da4721ec6148.png">

OpTree:

<img width="1076" alt="image"
src="https://user-images.githubusercontent.com/32269413/221405305-e1d53f94-5f97-4c11-a20c-c167fdd9d45b.png">






- [x] I have raised an issue to propose this change
([required](https://envpool.readthedocs.io/en/latest/pages/contributing.html)
for new features and bug fixes)

## Types of changes

What types of changes does your code introduce? Put an `x` in all the
boxes that apply:

- [x] Bug fix (non-breaking change which fixes an issue)
- [x] New feature (non-breaking change which adds core functionality)
- [x] Breaking change (fix or feature that would cause existing
functionality to change)
- [ ] Example (update in the folder of example)

## Checklist

Go over all the following points, and put an `x` in all the boxes that
apply.
If you are unsure about any of these, don't hesitate to ask. We are here
to help!

- [x] I have read the
[CONTRIBUTION](https://envpool.readthedocs.io/en/latest/pages/contributing.html)

envpool/python/gym_envpool.py
@@ -18,7 +18,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 import gym
 import numpy as np
-import treevalue
+import optree
 from packaging import version
 
 from .data import gym_structure
@@ -52,6 +52,7 @@ class GymEnvPoolMeta(ABCMeta, gym.Env.__class__):
     base = parents[0]
     try:
       from .lax import XlaMixin
+
       parents = (base, GymEnvPoolMixin, EnvPoolMixin, XlaMixin, gym.Env)
     except ImportError:
 
@@ -68,31 +69,28 @@ class GymEnvPoolMeta(ABCMeta, gym.Env.__class__):
     check_key_duplication(name, "state", state_keys)
     check_key_duplication(name, "action", action_keys)
 
-    tree_pairs = gym_structure(state_keys)
-    state_idx = list(zip(*tree_pairs))[-1]
+    state_paths, state_idx, treepsec = gym_structure(state_keys)
 
     new_gym_api = version.parse(gym.__version__) >= version.parse("0.26.0")
 
     def _to_gym(
       self: Any, state_values: List[np.ndarray], reset: bool, return_info: bool
     ) -> Union[Any, Tuple[Any, Any], Tuple[Any, np.ndarray, np.ndarray, Any],
-               Tuple[Any, np.ndarray, np.ndarray, np.ndarray, Any]]:
-      values = map(lambda i: state_values[i], state_idx)
-      state = treevalue.unflatten(
-        [(path, vi) for (path, _), vi in zip(tree_pairs, values)]
-      )
+               Tuple[Any, np.ndarray, np.ndarray, np.ndarray, Any],]:
+      values = (state_values[i] for i in state_idx)
+      state = optree.tree_unflatten(treepsec, values)
       if reset and not (return_info or new_gym_api):
-        return state.obs
-      info = treevalue.jsonify(state.info)
+        return state["obs"]
+      info = state["info"]
       if not new_gym_api:
-        info["TimeLimit.truncated"] = state.trunc
-      info["elapsed_step"] = state.elapsed_step
+        info["TimeLimit.truncated"] = state["trunc"]
+      info["elapsed_step"] = state["elapsed_step"]
       if reset:
-        return state.obs, info
+        return state["obs"], info
       if new_gym_api:
-        terminated = state.done & ~state.trunc
-        return state.obs, state.reward, terminated, state.trunc, info
-      return state.obs, state.reward, state.done, info
+        terminated = state["done"] & ~state["trunc"]
+        return state["obs"], state["reward"], terminated, state["trunc"], info
+      return state["obs"], state["reward"], state["done"], state["trunc"], info
 
     attrs["_to"] = _to_gym
     subcls = super().__new__(cls, name, parents, attrs)

envpool/python/BUILD
@@ -38,7 +38,7 @@ py_library(
     name = "data",
     srcs = ["data.py"],
     deps = [
-        requirement("treevalue"),
+        requirement("optree"),
         requirement("dm-env"),
         requirement("gym"),
         requirement("gymnasium"),
@@ -64,7 +64,7 @@ py_library(
     name = "envpool",
     srcs = ["envpool.py"],
     deps = [
-        requirement("treevalue"),
+        requirement("optree"),
         requirement("dm-env"),
         requirement("numpy"),
         requirement("packaging"),
@@ -97,7 +97,7 @@ py_library(
     name = "dm_envpool",
     srcs = ["dm_envpool.py"],
     deps = [
-        requirement("treevalue"),
+        requirement("optree"),
         requirement("dm-env"),
         requirement("numpy"),
         ":data",
@@ -111,7 +111,7 @@ py_library(
     name = "gym_envpool",
     srcs = ["gym_envpool.py"],
     deps = [
-        requirement("treevalue"),
+        requirement("optree"),
         requirement("dm-env"),
         requirement("gym"),
         requirement("numpy"),
@@ -126,7 +126,7 @@ py_library(
     name = "gymnasium_envpool",
     srcs = ["gymnasium_envpool.py"],
     deps = [
-        requirement("treevalue"),
+        requirement("optree"),
         requirement("dm-env"),
         requirement("gymnasium"),
         requirement("numpy"),

envpool/python/data.py
@@ -20,7 +20,8 @@ import dm_env
 import gym
 import gymnasium
 import numpy as np
-import treevalue
+import optree
+from optree import PyTreeSpec
 
 from .protocol import ArraySpec
 
@@ -124,8 +125,10 @@ def gymnasium_spec_transform(
   )
 
 
-def dm_structure(root_name: str,
-                 keys: List[str]) -> List[Tuple[List[str], int]]:
+def dm_structure(
+  root_name: str,
+  keys: List[str],
+) -> Tuple[List[Tuple[int, ...]], List[int], PyTreeSpec]:
   """Convert flat keys into tree structure for namedtuple construction."""
   new_keys = []
   for key in keys:
@@ -135,16 +138,19 @@ def dm_structure(root_name: str,
     key = key.replace("obs:", f"{root_name}:")  # compatible with to_namedtuple
     new_keys.append(key.replace(":", "."))
   dict_tree = to_nested_dict(dict(zip(new_keys, list(range(len(new_keys))))))
-  tree_pairs = treevalue.flatten(treevalue.TreeValue(dict_tree))
-  return tree_pairs
+  structure = to_namedtuple(root_name, dict_tree)
+  paths, indices, treespec = optree.tree_flatten_with_path(structure)
+  return paths, indices, treespec
 
 
-def gym_structure(keys: List[str]) -> List[Tuple[List[str], int]]:
+def gym_structure(
+  keys: List[str]
+) -> Tuple[List[Tuple[str, ...]], List[int], PyTreeSpec]:
   """Convert flat keys into tree structure for dict construction."""
   keys = [k.replace(":", ".") for k in keys]
-  structure = to_nested_dict(dict(zip(keys, list(range(len(keys))))))
-  tree_pairs = treevalue.flatten(treevalue.TreeValue(structure))
-  return tree_pairs
+  dict_tree = to_nested_dict(dict(zip(keys, list(range(len(keys))))))
+  paths, indices, treespec = optree.tree_flatten_with_path(dict_tree)
+  return paths, indices, treespec
 
 
 gymnasium_structure = gym_structure

envpool/python/dm_envpool.py
@@ -18,7 +18,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 import dm_env
 import numpy as np
-import treevalue
+import optree
 from dm_env import TimeStep
 
 from .data import dm_structure
@@ -50,6 +50,7 @@ class DMEnvPoolMeta(ABCMeta):
     base = parents[0]
     try:
       from .lax import XlaMixin
+
       parents = (
         base, DMEnvPoolMixin, EnvPoolMixin, XlaMixin, dm_env.Environment
       )
@@ -68,8 +69,7 @@ class DMEnvPoolMeta(ABCMeta):
     check_key_duplication(name, "state", state_keys)
     check_key_duplication(name, "action", action_keys)
 
-    tree_pairs = dm_structure("State", state_keys)
-    state_idx = list(zip(*tree_pairs))[-1]
+    state_paths, state_idx, treepsec = dm_structure("State", state_keys)
 
     def _to_dm(
       self: Any,
@@ -77,10 +77,8 @@ class DMEnvPoolMeta(ABCMeta):
       reset: bool,
       return_info: bool,
     ) -> TimeStep:
-      values = map(lambda i: state_values[i], state_idx)
-      state = treevalue.unflatten(
-        [(path, vi) for (path, _), vi in zip(tree_pairs, values)]
-      )
+      values = (state_values[i] for i in state_idx)
+      state = optree.tree_unflatten(treepsec, values)
       timestep = TimeStep(
         step_type=state.step_type,
         observation=state.State,

envpool/python/envpool.py
@@ -19,7 +19,7 @@ from abc import ABC
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
-import treevalue
+import optree
 from dm_env import TimeStep
 
 from .protocol import EnvPool, EnvSpec
@@ -59,9 +59,8 @@ class EnvPoolMixin(ABC):
   ) -> List[np.ndarray]:
     """Convert action to C++-acceptable format."""
     if isinstance(action, dict):
-      atree = treevalue.TreeValue(action)
-      alist = treevalue.flatten(atree)
-      adict = {".".join(k): v for k, v in alist}
+      paths, values, _ = optree.tree_flatten_with_path(action)
+      adict = {'.'.join(p): v for p, v in zip(paths, values)}
     else:  # only 3 keys in action_keys
       if not hasattr(self, "_last_action_type"):
         self._last_action_type = self._spec._action_spec[-1][0]

envpool/python/gymnasium_envpool.py
@@ -18,7 +18,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 import gymnasium
 import numpy as np
-import treevalue
+import optree
 
 from .data import gymnasium_structure
 from .envpool import EnvPoolMixin
@@ -70,23 +70,20 @@ class GymnasiumEnvPoolMeta(ABCMeta, gymnasium.Env.__class__):
     check_key_duplication(name, "state", state_keys)
     check_key_duplication(name, "action", action_keys)
 
-    tree_pairs = gymnasium_structure(state_keys)
-    state_idx = list(zip(*tree_pairs))[-1]
+    state_paths, state_idx, treepsec = gymnasium_structure(state_keys)
 
     def _to_gymnasium(
       self: Any, state_values: List[np.ndarray], reset: bool, return_info: bool
     ) -> Union[Any, Tuple[Any, Any], Tuple[Any, np.ndarray, np.ndarray, Any],
-               Tuple[Any, np.ndarray, np.ndarray, np.ndarray, Any]]:
-      values = map(lambda i: state_values[i], state_idx)
-      state = treevalue.unflatten(
-        [(path, vi) for (path, _), vi in zip(tree_pairs, values)]
-      )
-      info = treevalue.jsonify(state.info)
-      info["elapsed_step"] = state.elapsed_step
+               Tuple[Any, np.ndarray, np.ndarray, np.ndarray, Any],]:
+      values = (state_values[i] for i in state_idx)
+      state = optree.tree_unflatten(treepsec, values)
+      info = state["info"]
+      info["elapsed_step"] = state["elapsed_step"]
       if reset:
-        return state.obs, info
-      terminated = state.done & ~state.trunc
-      return state.obs, state.reward, terminated, state.trunc, info
+        return state["obs"], info
+      terminated = state["done"] & ~state["trunc"]
+      return state["obs"], state["reward"], terminated, state["trunc"], info
 
     attrs["_to"] = _to_gymnasium
     subcls = super().__new__(cls, name, parents, attrs)

setup.cfg
@@ -31,7 +31,7 @@ install_requires =
     types-protobuf>=3.17.3
     typing-extensions
     packaging
-    treevalue>=1.4.7
+    optree>=0.6.0
 
 [options.packages.find]
 include = envpool*

commit 8616a6ff6bfe1b656e028a1c87604b5780dc10e5

fix old gym version return type error (#254)
This fixes a bug introduced by #247

envpool/python/gym_envpool.py
@@ -90,7 +90,7 @@ class GymEnvPoolMeta(ABCMeta, gym.Env.__class__):
       if new_gym_api:
         terminated = state["done"] & ~state["trunc"]
         return state["obs"], state["reward"], terminated, state["trunc"], info
-      return state["obs"], state["reward"], state["done"], state["trunc"], info
+      return state["obs"], state["reward"], state["done"], info
 
     attrs["_to"] = _to_gym
     subcls = super().__new__(cls, name, parents, attrs)