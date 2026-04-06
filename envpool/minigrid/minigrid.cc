4461642926c9a3fa1f21e0645f0476e4bf6d6868
Jiayi Weng <jiayi@openai.com>
2026-04-06 09:25:38 -0700

    [minigrid] inline bindings into pybind extension

--- envpool/minigrid/minigrid.cc
+++ envpool/minigrid/minigrid.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "envpool/minigrid/minigrid.h"
-
 #include <pybind11/pybind11.h>
+
+#include "envpool/minigrid/minigrid.h"
 
 namespace py = pybind11;
 
